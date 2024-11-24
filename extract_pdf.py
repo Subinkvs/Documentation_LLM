4d96e0bad3427834db59e3ca96bc761affd681ed
SubinSundar <subinsundar9624@gmail.com>
2024-11-24 15:16:27 +0530

    added summarizer

--- extract_pdf.py
+++ extract_pdf.py
@@ -1,4 +1,5 @@
 import pdfplumber
+from transformers import pipeline
 
 pdf_path = "C:/Users/USER/Downloads/google_terms_of_service_en_in.pdf"
 
@@ -12,11 +13,16 @@ with pdfplumber.open(pdf_path) as pdf:
 with open(output_text_file, "w") as text_file:
     text_file.write(extracted_text)
     
-print(f"Text extracted and saved to {output_text_file}")
+# print(f"Text extracted and saved to {output_text_file}")
 
 # Open and read the extracted text file
 with open(output_text_file, "r") as text_file:
     extracted_text = text_file.read()
 
 # Print the content of the text file
-print(extracted_text)
+# print(extracted_text[:500])
+
+summarizer = pipeline("summarization", model="t5-small")
+
+summary = summarizer(extracted_text[:1000], max_length=150, min_length=30, do_sample=False)
+print("Summary:", summary[0]['summary_text'])
